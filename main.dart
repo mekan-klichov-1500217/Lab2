c26192056be9183a7ba7eb4f92c5a8ff61a311de
Mekan Klichov <m.klichov@newuzbekistanuniversity.uz>
2025-09-09 14:18:09 +0500

    commit1

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  //T2
+  print("Mekan Klichov");
+
+  //T3
+  print("Welcome to Dart Programming");
+  print("Let's start coding!");
+
+  //T4
+  print("1 Amir Temur Avenue \nTashkent, Uzbekistan \n100000");
+}
